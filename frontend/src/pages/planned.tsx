2a9f6bb8db4584b20ad43b2c7b9ad1719c5702bb
roeytamano <roitamano@mail.com>
2025-05-25 17:46:33 +0300

    refactor: update OMDB API URL to use HTTPS for secure requests

--- frontend/src/pages/planned.tsx
+++ frontend/src/pages/planned.tsx
@@ -46,7 +46,7 @@ const Planned = () => {
       const details = await Promise.all(
         plannedMovies.map(async (movieId) => {
           try {
-            const response = await fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=2cc58735`);
+            const response = await fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=2cc58735`);
             const data = await response.json();
             return data;
           } catch (err) {
