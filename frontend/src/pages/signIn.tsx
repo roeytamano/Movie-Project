57056cb9b04d27846a2f0538c30e3cdb14495c5b
roeytamano <roitamano@mail.com>
2025-05-25 19:57:14 +0300

    revert to commit e2dfb7d168b00e6cc8c48decccb98e1c8ae1566f

--- frontend/src/pages/signIn.tsx
+++
@@ -1,9 +0,0 @@
-import Sign from "../components/Sign"
-
-const SignIn = () => {
-    return (
-        <Sign title="Sign In" signIn={true}/> 
-    )
-  };
-  
-  export default SignIn;
--- frontend/src/pages/signUp.tsx
+++
@@ -1,9 +0,0 @@
-import Sign from "../components/Sign"
-
-const SignOut = () => {
-    return (
-        <Sign title="Sign Up" signIn={false}/> 
-    )
-  };
-  
-  export default SignOut;
